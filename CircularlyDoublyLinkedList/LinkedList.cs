c56e40fbd7c971e72b343299a6eee0e073e36c73
Liron Saedi <lionsaedi@gmail.com>
2019-08-07 17:32:51 -0700

    initial upload of circularlydoublylinkedlist

---
+++ CircularlyDoublyLinkedList/LinkedList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircularlyDoublyLinkedList
+{
+    class LinkedList<T>
+    {
+        Node<T> Head;
+        Node<T> Tail; 
+
+        public void AddFirst(T value)
+        {
+            if (Head == null)
+            {
+                Head = new Node<T>(value);
+                return;
+            }
+
+            Node<T> Temp = new Node<T>(value);
+
+            if (Tail == null)
+            {
+                Tail = Head;
+            }
+        }
+
+
+        public void AddLast()
+        {
+
+        }
+
+        public void AddBefore()
+        {
+
+        }
+
+        public void AddAfter()
+        {
+
+        }
+
+        public bool RemoveFirst()
+        {
+            return false;
+        }
+
+        public bool RemoveLast()
+        {
+            return false;
+        }
+
+        public bool Remove(T value)
+        {
+            return false;
+        }
+
+        public bool IsEmpty()
+        {
+            if (Head == null)
+            {
+                return true;
+            }
+
+            return false;
+            
+        }
+    }
+}
